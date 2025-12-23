6541d5b307655ed23eca5d0ca2bdd3c82d995151
aditiksharma6321-netizen <aditiksharma6321@gmail.com>
2025-12-23 14:49:18 +0530

    using external module

--- python1.py
+++ python1.py
@@ -1,25 +1,5 @@
-# program to print twinkle twinkle poem
-print('''Twinkle, twinkle, little star,
-How I wonder what you are!
-Up above the world so high,
-Like a diamond in the sky.
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("aditi is such a dumb girl")
+engine.runAndWait()
 
-When the blazing sun is gone,
-When he nothing shines upon,
-Then you show your little light,
-Twinkle, twinkle, all the night.
-
-Then the trav'ller in the dark,
-Thanks you for your tiny spark,
-He could not see which way to go,
-If you did not twinkle so.
-
-In the dark blue sky you keep,
-And often thro' my curtains peep,
-For you never shut your eye,
-Till the sun is in the sky.
-
-'Tis your bright and tiny spark,
-Lights the trav'ller in the dark:
-Tho' I know not what you are,
-Twinkle, twinkle, little star''')
