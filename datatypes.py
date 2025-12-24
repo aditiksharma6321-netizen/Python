6fa75a18660b100a8409adb30c40f0038273d05a
aditiksharma6321-netizen <aditiksharma6321@gmail.com>
2025-12-24 12:54:07 +0530

    variables

---
+++ datatypes.py
@@ -0,0 +1,5 @@
+a=1 # a is a integer datatype
+b=33.3 #b is a floating point number
+c="aditi" # c is a string datatypes
+d = True # d is a boolean datatype
+e= None # e is a none datatypes
--- python1.py
+++ python1.py
@@ -1,5 +1,5 @@
 import pyttsx3
 engine = pyttsx3.init()
-engine.say("aditi is such a dumb girl")
+engine.say("nisha is my friend")
 engine.runAndWait()
 
